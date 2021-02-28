4603aaae4aa1a843ece093dbc19cb5f7e67e7c46
TruongMac <truong.mac@sotatek.com>
2021-02-28 08:39:58 +0700

    Add antd for project

--- src/views/App.js
+++ src/views/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import createRoutes from './router';
 import store from './../state/configureStore';
+import 'antd/dist/antd.css';
 
 class App extends React.Component {
   render() {
